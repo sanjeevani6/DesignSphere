989674cdf4d47e8e5721023cee9d52763a82abfa
sanjeevani6 <jaiswalsanjeevani4@gmail.com>
2024-10-27 20:35:24 +0530

    coonection to user database established and routes updated

---
+++ controllers/userController.js
@@ -0,0 +1,46 @@
+const usermodel = require('../models/usermodels')
+
+
+
+//login controller
+const logincontroller = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const user = await usermodel.findOne({ email, password })
+        if (!user) {
+           return  res.status(404).send("User not found")
+        }
+        return  res.status(200).json({
+            success: true,
+            user,
+        })
+    }
+    catch (error) {
+        return  res.status(400).json({
+            success: false,
+            error,
+        });
+    }
+};
+
+
+//register callback
+const registercontroller = async (req, res) => {
+    try {
+        const newuser = new usermodel(req.body);
+        await newuser.save();
+       res.status(201).json({
+            success: true,
+            newuser,
+        });
+
+    }
+    catch (error) {
+      res.status(400).json({
+            success: false,
+            error:error.message,
+        })
+    }
+};
+
+module.exports = { logincontroller, registercontroller };
---
+++ routes/userRoute.js
@@ -0,0 +1,22 @@
+const express=require('express')
+const { logincontroller, registercontroller } = require('../controllers/usercontrollers')
+//router object
+const router =express.Router();
+
+//routers
+
+router.get('/test', (req, res) => {
+    console.log("test route worked");
+    res.send('Test route works');
+});
+//post||login
+
+router.post('/login',logincontroller);
+
+//post||register user
+router.post('/register',registercontroller);
+
+
+
+
+module.exports=router;
